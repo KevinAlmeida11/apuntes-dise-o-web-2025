f4be746d2702b9c3590db90dcd3ad68a6bdbdcac
Cande <almeida_bazaldua@alumnos.cei.es>
2025-03-20 21:56:16 +0100

    66967876896789679

--- KEVIN ALMEIDA BAZALDUA/APUNTES DE DISEÑO WEB/dia35/actividad7/profemusica.js
+++ KEVIN ALMEIDA BAZALDUA/APUNTES DE DISEÑO WEB/dia35/actividad7/profemusica.js
@@ -3,7 +3,9 @@ const listaBloques = document.querySelectorAll(".tab-content");
 const listaButtons = document.querySelectorAll(".nav-button");
 const containerCanciones=document.getElementById("containerCanciones")
 
-const canciones = [
+let idCancionActual=0;
+
+const listaTemas = [
     {
         id: 1,
         titulo: "Canción 1",
@@ -81,16 +83,78 @@ listaTemas.forEach(tema=>{
 
     const { id,titulo,artista,imagen,fuente}=tema;
     console.log(titulo);
-})
+});
 
 //usamos innerHTML para crear nuestros temas
-containerCanciones.innerHTML+=`<li class="cardCancion"> 
-                    <img src="./img/tema1.jpg" alt="Mickael Jackson"> 
+containerCanciones.innerHTML+=`<li class="cardCancion" data-id=${id}> 
+                    <img class="imgCancion"src="./${imagen}" alt="${artista}"> 
                     <div> 
-                         <h2>Wanna Be Startin 'Somethin'</h2> 
-                        <small class="artista"></small> 
+                         <h2 class="titleCancion">${titulo}'</h2> 
+                        <small class="artistaCancion">${artista}</small> 
                 </div> 
-                </li> `
+                </li> `;
+
+//agarrar el ultimo hijo agregado para incluirle un eventLsitener
+
+const cardCancion=containerCanciones.lastChild;
+console.log("card:",cardCanciones);
+console.log(titulo);
+
+cardCancion.addEventListener("click", ()=>{
+    cargarCancion(id);
+
+    //opcional:pasar a la pestaña de reproducion
+    document.querySelector("[data-tab='tab3']").click();
+});
+
 
 
 //---------------REPRODUCTOR-----------------//
+const playBtn=document.querySelector(".fa-play");
+const pauseBtn=document.querySelector(".fa-pause");
+const nextBtn=document.querySelector(".fa-step-forward");
+const prevBtn=document.querySelector(".fa-step-backward");
+const audioPlayer=document.querySelector("#audioPlayer");//nuestro reproductor
+
+const divCurrentSongInfo=document.querySelector(".current-song-info");
+
+pauseBtn.style.display="none";
+
+
+//funcion para cargar un tema
+function cargaCancion(id){
+    //recorremos nuestras canciones y obtenemos solo la que me interesa
+
+    const cancion=listaTemas.find(tema.id==id);
+
+    if(!cancion){return;}
+
+    //deconstruccion de objetos para obtener sus variables
+    const {titulo,imagen,artista,fuente}=cancion;
+
+    console.log(cancion);
+    audioPlayer.src=cancion.fuente;
+    idCancionActual=id;
+
+divCurrentSongInfo.innerHTML=
+
+                <img src="./${imagen}" alt="${artista}" style="width:200px"></img>
+                
+    
+
+
+    audioPlayer.play();
+    playBtn.style.display="none";
+    pauseBtn.style.display="inline";
+}
+
+//Event listener de botones de reproductor
+playBtn.addEventListener("click",()=>{});
+
+
+nextBtn.addEventListener("click",()=>{
+    posicionTemaActual++;
+    console.log("Posicion es:", posicionTemaActual)
+    const idCancion=listaTemas[posicionTemaActual].id;
+    cargarCancion(idCancion);
+});
---
+++ KEVIN ALMEIDA BAZALDUA/APUNTES DE DISEÑO WEB/dia35/lightbox/lightboxClase.js
@@ -0,0 +1,10 @@
+const listaEnlaces=document.querySelectorAll(".a");
+const lightbox=document.querySelector(".lightbox");
+const cerrar=document.querySelector(".btnCerrar");
+const imgBig=document.querySelector(".imgBig");
+//recorrer los enlaces
+//agregar un eventlistener a cada uno
+//agregar la clase active a lightbox
+//imgBig cambiarle el "src" por la imagen a la que hice click.
+
+//si hago click en cerrar... sacar clase de active de lightbox.
